cb8a4b81f36f457ca469abc98bbe33f7554d4985
Haris Raees <haris.pixako1@gmail.com>
2025-10-17 10:51:14 +0500

    Initial commit - my first PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+import withPWA from 'next-pwa';
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode :true
+};
 
-export default nextConfig;
+const pwaConfig = withPWA({
+  dest: 'public', // Destination folder for PWA fil
+  register: true, // Register the service worker
+  skipWaiting: true, // Activate the new service wo
+  // Add other next-pwa options as needed, e.g., di
+});
+
+export default pwaConfig(nextConfig);
